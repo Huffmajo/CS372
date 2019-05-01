5a2be37758eb43a96fbf75c711057c84d4f42f3d
Huffmajo <huffmajo@oregonstate.edu>
2019-04-30 19:45:34 -0700

    Chat server mostly working. Non-graceful exit on ctrl+c.

--- project1/chatserve.py
+++ project1/chatserve.py
@@ -1,50 +1,80 @@
 ##########################################################
 # Program: chatserve.py
 # Author: Joel Huffman
-# Last updated: 4/28/2019
+# Last updated: 4/30/2019
 # Sources: https://media.oregonstate.edu/media/t/0_hh05nevl
 # https://docs.python.org/release/2.6.5/library/internet.html
 # https://beej.us/guide/bgnet/html/single/bgnet.html
 ##########################################################
 
 from socket import *
+import sys
 
-##########################################################
-# Function: setupServer()
 # 
-def setupServer():
-	# get port number
+def SendAndRecv(connectionSocket):
 
+	# get username from client
+	clientUserName = connectionSocket.recv(1024)
+	
+	# take turns receiving and sending ad infinitum
 	while 1:
-		print "Please enter an integer between 1024-65535 to use as a port number:"
 
-		serverPort = input()
-		if (serverPort.isdigit() == false):
-			print "Input is not an integer. Try again."
-		elif (serverPort > 65535 or serverPort < 1024):
-			print "Port number is not in the accepted range. Try again."
-		else:
+		# get message from client
+		recvMsg = connectionSocket.recv(1024)
+	
+		# print received message
+		f"{clientUserName}> {recvMsg}"
+
+		# let user type message to send to client
+		print("Server> ")
+		sendMsg = input()
+
+		# allow disconnection if quit command is used
+		if sendMsg == "\quit":
+			print("Connection closed")
 			break
 
-# greet user
-print "***Welcome to ChatServe***"
-
-serverPort
-
-while 1:
-	while 1:
-		setupServer();
-
+		# otherwise, send the message to client
+		connectionSocket.send(sendMsg)		
 	
 
+# main function
+if __name__ == "__main__":
+	# greet user
+	print ("***Welcome to ChatServe***")
 
+	# check that potential port number was included in call
+	if len(sys.argv) != 2:
+		print ("Specify port number with chatserve.py call.")
+		exit(1)
+
+	# get port number
+	serverPort = sys.argv[1]
+
+	# verify port number
+	if serverPort.isdigit() == False:
+		print ("Included argument is not an integer. Please call charserve.py with a port number from 1024-65535.")
+		exit(1)
+
+	# get hostname
+	serverHost = gethostname()
+
+	# setup server socket
 	serverSocket = socket(AF_INET, SOCK_STREAM)
-	serverSocket.bind(('', serverport))
+
+	# bind socket to port number
+	serverSocket.bind((serverHost, int(serverPort)))
+
+	# listen at bound port for client
 	serverSocket.listen(1)
-	print "The server is ready to receive"
-	# while 1:
-	#	connectionSocket, addr = serverSocket.accept()
-	#	sentence = connectionSocket.recv(1024)
-	#	capitalizedSentence = sentence.upper()
-	#	connectionSocket.send(capitalizedSentence)
-	#	connectionSocket.close()
+
+	print ("The server is ready to receive")
+	
+	# keep connection open until ctrl + c interrupt
+	while 1:
+		connectionSocket, addr = serverSocket.accept()
+		f"Connected on {addr}"
+
+		SendAndRecv(connectionSocket)	
+
+		connectionSocket.close()
